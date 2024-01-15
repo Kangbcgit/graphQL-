909b366e2f5c52519f0f4932d200b16bedec5bf6
강병찬 <kangbcgit@gangbyeongchan-ui-MacBookPro.local>
2024-01-15 20:10:38 +0900

    #1.0 setup complete

---
+++ src/apolloClient.js
@@ -0,0 +1,18 @@
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000", //graphQL서버 주소
+  cache: new InMemoryCache(), // 준 필수적인 .. 나중에 다룰 것
+});
+
+// client.query({ //test code (successfully result)
+//   query: gql`
+//   {
+//     allMovies {
+//       title_english
+//     }
+//   }
+//   `
+// }).then(data => console.log(data));
+
+export default client
---
+++ src/routes/Movies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Movies() {
+  return (
+    <div>
+      This is movie detail.
+    </div>
+  )
+}
+
+export default Movies
